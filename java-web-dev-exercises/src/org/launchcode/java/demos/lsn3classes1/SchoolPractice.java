66d5f98cff1c347ed03c34b18176d7fb1fa7fb6d
MSchwartz <mschwartz@clubready.com>
2020-08-31 18:39:48 -0500

    new code

--- java-web-dev-exercises/src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ java-web-dev-exercises/src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,10 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student zupa = new Student(name: "Zupa Schwartz", studentId: 1232456, numberOfCredits: 1, gpa: 4.0);
+        Student alex = new Student(name: "Alex Cole", studentId: 603275);
+
+        System.out.println("Zupa's gpa:" + zupa.getGpa());
+        System.out.println("Alex's gpa:" + alex.getGpa());
     }
 }
--- java-web-dev-exercises/src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ java-web-dev-exercises/src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,40 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = 0;
+        this.gpa = 0.0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentId() {
+        return this.studentId;
+    }
+
+    public  int getNumberOfCredits() {
+        return this.numberOfCredits;
+    }
+
+    public double getGpa() {
+        return this.gpa;
+    }
+    public static void
+
 }
